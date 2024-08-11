7c3db77ef9862162d54952d00a775eb7abd70259
shash0423 <shash042319@naver.com>
2024-08-11 15:00:28 +0900

    [FEAT] 회원 조회 API

--- src/main/java/daenamoo/homepage/api/MemberApiController.java
+++ src/main/java/daenamoo/homepage/api/MemberApiController.java
@@ -2,10 +2,13 @@ package daenamoo.homepage.api;
 
 import daenamoo.homepage.domain.Member;
 import daenamoo.homepage.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,10 +20,8 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-
-
     @GetMapping("/members")
-    public Result memberV2() {
+    public Result findMembers() {
         List<Member> findMembers = memberService.findMembers();
         List<MemberDto> collect = findMembers.stream()
                 .map(m -> new MemberDto(m.getStudentId(), m.getName(), m.getMajor(), m.isAdmin()))
@@ -28,6 +29,22 @@ public class MemberApiController {
 
         return new Result(collect);
     }
+
+    @GetMapping("/members/{id}")
+    public Result findMember(
+            @PathVariable("id") Long id
+    ) {
+        Member findMember = memberService.findOne(id);
+        MemberDto memberDto = new MemberDto(
+                findMember.getStudentId(),
+                findMember.getName(),
+                findMember.getMajor(),
+                findMember.isAdmin()
+        );
+
+        return new Result(memberDto);
+    }
+
 
     @Data
     @AllArgsConstructor
